c3efb1da58834003d0e12daae276d937df5e3346
damianduralek <damian.duralek@gmail.com>
2021-03-07 19:18:45 +0100

    test

---
+++ Zadanie_1/script.ts
@@ -0,0 +1,11 @@
+class InputsApp{
+    input1:number;
+    input2:number;
+    input3:number;
+    input4:number;
+    sum:number;
+    avg:number;
+    min:number;
+    max:number;
+    constructor(){}
+}
